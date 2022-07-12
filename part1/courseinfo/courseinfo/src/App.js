c3b8df8f5178080eb87ed7674d701821af401a3c
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-07-12 14:55:33 -0500

    Complete exercises 1.1-1.2

--- part1/courseinfo/courseinfo/src/App.js
+++ part1/courseinfo/courseinfo/src/App.js
@@ -9,15 +9,18 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p><p>
-        {props.part2} {props.exercises2}
-      </p><p>
-        {props.part3} {props.exercises3}
-      </p>
-      </>
+    <div>
+    <Part name={props.part1} exercise={props.exercises1 } />
+    <Part name={props.part2} exercise={props.exercises2} />
+    <Part name={props.part3} exercise={props.exercises3} />
+
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercise}</p>
   )
 }
 
