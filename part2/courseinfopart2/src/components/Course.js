16ba30a722e0958f5953e813eaca4995b0ef2abe
Ryan Bennett <Ryan.Bennett322@gmail.com>
2022-07-15 11:40:47 -0500

    Completed part2a exercises

---
+++ part2/courseinfopart2/src/components/Course.js
@@ -0,0 +1,20 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
+  
+const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part} />)
+
+const Course = ({ course }) => {
+
+    return (
+      <>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total sum={course.parts.reduce((s, part) => s + part.exercises,0)}/>
+      </>
+    )
+  }
+
+export default Course
